654a9c0e1f7a5a3013131e8ef225cef567079811
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2021-05-13 10:11:18 -0300

    ok

--- platforms/android/app/build/generated/source/buildConfig/debug/br/com/innovatesoft/biblianvi/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/br/com/innovatesoft/biblianvi/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "br.com.innovatesoft.biblianvi";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 10003;
-  public static final String VERSION_NAME = "1.0.3";
+  public static final int VERSION_CODE = 10004;
+  public static final String VERSION_NAME = "1.0.4";
 }
